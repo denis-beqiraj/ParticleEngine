2aceb521e6311a33c58e00935fde1994884c21bf
denis-beqiraj <114519777+denis-beqiraj@users.noreply.github.com>
2023-06-01 19:15:36 +0200

    Added current

--- demo/main.cpp
+++ demo/main.cpp
@@ -132,17 +132,6 @@ void createParticles(int maxParticles) {
     }
 }
 
-void updateParticles() {
-    for (int i = 0; i < particles->size(); i++) {
-        particles->at(i).initPosition = glm::vec4(0.0f);
-        particles->at(i).initVelocity = glm::vec4(((float)rand() / RAND_MAX) * startVelocity.x - startVelocity.x / 2, ((float)rand() / RAND_MAX) * startVelocity.y - startVelocity.y / 2, ((float)rand() / RAND_MAX) * startVelocity.z - startVelocity.z / 2, 0.0f);
-        particles->at(i).initAcceleration = glm::vec4(startAcceleration, 0.0f);
-        particles->at(i).initLife = ((float)rand() / RAND_MAX) * initLife.x;
-        particles->at(i).minLife = -((float)rand() / RAND_MAX) * initLife.y;
-        particles->at(i).color = glm::vec4(color, 1.0f);
-    }
-}
-
 //////////
 // MAIN //
 //////////
@@ -249,17 +238,17 @@ int main(int argc, char *argv[])
          }
          eng.getImgui()->newText("Start velocity");
          if (eng.getImgui()->newBar("XV", startVelocity.x, -100.0f, 100.0f) | eng.getImgui()->newBar("YV", startVelocity.y, -100.0f, 100.0f) | eng.getImgui()->newBar("ZV", startVelocity.z, -100.0f, 100.0f)) {
-             updateParticles();
+             createParticles(value);
              particleEmitter.setParticles(particles);
          }
          eng.getImgui()->newText("Start acceleration");
          if (eng.getImgui()->newBar("XA", startAcceleration.x, -100.0f, 100.0f) | eng.getImgui()->newBar("YA", startAcceleration.y, -100.0f, 100.0f) | eng.getImgui()->newBar("ZA", startAcceleration.z, -100.0f, 100.0f)) {
-             updateParticles();
+             createParticles(value);
              particleEmitter.setParticles(particles);
          }
          eng.getImgui()->newText("Life");
          if (eng.getImgui()->newBar("Init life", initLife.x, -100.0f, 100.0f) | eng.getImgui()->newBar("End life", initLife.y, -100.0f, 100.0f)) {
-             updateParticles();
+             createParticles(value);
              particleEmitter.setParticles(particles);
          }
          eng.getImgui()->render();
